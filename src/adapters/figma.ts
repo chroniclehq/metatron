278fe7f45cfdddf613e267c2f09b220451db089a
harris <harris@chroniclehq.com>
2023-06-01 15:27:25 +0530

    Tweaks to figma

--- src/adapters/figma.ts
+++ src/adapters/figma.ts
@@ -1,6 +1,5 @@
 import got from 'got';
-import { pick } from 'lodash-es';
-import Generic from './generic.js';
+import { pick, mapKeys, camelCase } from 'lodash-es';
 
 const URL_REGEX =
   /https:\/\/([\w\.-]+\.)?figma.com\/(file|proto)\/([0-9a-zA-Z]{22,128})(?:\/.*)?$/;
@@ -26,31 +25,33 @@ export default class Figma {
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), 5000);
 
-    const meta = await got(oembedUrl.toString(), {
-      signal: controller.signal,
-      headers: {
-        'User-Agent': 'chronicle-bot/1.0',
-      },
-      resolveBodyOnly: true,
-      responseType: 'json',
-    })
-      .then((res: JSON) => {
-        return pick(res, [
-          'title',
-          'provider_name',
-          'folder_name',
-          'thumbnail_url',
-          'thumbnail_height',
-          'thumbnail_width',
-        ]);
-      })
-      .catch(async () => {
-        return await new Generic(this.url).fetchMeta();
-      })
-      .finally(() => {
-        clearTimeout(timeoutId);
+    const props = [
+      'title',
+      'provider_name',
+      'folder_name',
+      'thumbnail_url',
+      'thumbnail_height',
+      'thumbnail_width',
+    ];
+
+    try {
+      const res: JSON = await got(oembedUrl.toString(), {
+        signal: controller.signal,
+        headers: {
+          'User-Agent': 'chronicle-bot/1.0',
+        },
+        resolveBodyOnly: true,
+        responseType: 'json',
       });
 
-    return meta;
+      const meta = mapKeys(pick(res, props), (_, k) => camelCase(k));
+
+      return meta;
+    } catch (error) {
+      console.error(error);
+      return {};
+    } finally {
+      clearTimeout(timeoutId);
+    }
   }
 }
